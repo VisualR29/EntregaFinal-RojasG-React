14f9c74d007058dd5b5d3f5ec3187dafb3c5a6ab
Esteban <visualdev29@gmail.com>
2024-02-01 22:49:31 -0600

    estructura cuadros

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -5,15 +5,16 @@ import excel from "./../../img/excel.jpeg";
 
 function ItemListContainer() {
     return <>
-            <section className="Hero">
-            
-            </section>
-            <section className="Columns Itemlist">
-                <ProfileCard titulo={"Finanzas"} descripcion={"Certificado: 12 semanas"} precio={7200} img={finanzas} className="Column"/>
-                <ProfileCard titulo={"Excel"} descripcion={"Ruta"} precio={15000} img={excel} className="Column"/>
-                <ProfileCard titulo={"Ventas"} descripcion={"Certificado"} precio={6800} img={ventas} className="Column"/>
-            </section>;
-        </>
+        <section className="Hero">
+            <h2>Skilling Tec</h2>
+            <h3>El lugar donde las metas triunfan</h3>
+        </section>
+        <section className="Columns Itemlist">
+            <ProfileCard titulo={"Finanzas"} descripcion={"Certificado: 12 semanas"} precio={7200} img={finanzas} className="Column" />
+            <ProfileCard titulo={"Excel"} descripcion={"Ruta"} precio={15000} img={excel} className="Column" />
+            <ProfileCard titulo={"Ventas"} descripcion={"Certificado"} precio={6800} img={ventas} className="Column" />
+        </section>;
+    </>
 }
 
 export default ItemListContainer;
