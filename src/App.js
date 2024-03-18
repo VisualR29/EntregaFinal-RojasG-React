28695aca3d71968d9a7e824fb2e9fc8ea42673f3
Esteban <visualdev29@gmail.com>
2024-03-17 20:32:32 -0600

    Checkout

--- src/App.js
+++ src/App.js
@@ -4,34 +4,12 @@ import ItemListContainer from "./components/ItemListContainer/ItemListContainer"
 import ItemDetailContainer from "./components/ItemDetailContainer/ItemDetailContainer";
 import NotFound from "./components/NotFound/NotFound";
 import Layout from "./components/Layout";
+import Checkout from "./components/Checkout/Checkout.jsx"
 import { CartProvider } from "./context/CartContext";
 import Cart from "./components/Cart/Cart";
 
-// import { useEffect, useState } from "react";
-// import { db } from "../src/config/firebase";
-// import { collection } from "firebase/firestore";
-// import { getDocs } from "firebase/firestore";
 
 function App() {
-
-    // const [products, setProducts] = useState([]);
-
-    // const productReference = collection(db, "Items");
-
-    // useEffect(() => {
-    //     const getItems = async () => {
-
-    //         const data = await getDocs(productReference);
-
-    //         const filteredData = data.docs.map((doc) => ({
-    //             ...doc.data(),
-    //             id:doc.id
-    //         }))
-    //         setProducts(filteredData);
-    //     }
-
-    //     getItems();
-    // }, [])
 
     return (
         <div className="App">
@@ -43,6 +21,7 @@ function App() {
                             <Route path='/category/:categoryId' element={<ItemListContainer />} />
                             <Route path='/item/:itemId' element={<ItemDetailContainer />} />
                             <Route path='/cart' element={<Cart />} />
+                            <Route path='checkout' element={ <Checkout />} />
                             <Route path='*' element={<NotFound />} />
                         </Route>
                     </Routes>
